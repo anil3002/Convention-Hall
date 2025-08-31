e29d4ceeb4bbacc9c959cea300e8662c147e7fd5
Anil K <anil.k@culturalinfusion.org.au>
2025-08-31 16:02:15 +1000

    repo name checking

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/convention-hall/',
+  base: '/Convention-Hall/',
   plugins: [react()],
   resolve: {
     alias: {
